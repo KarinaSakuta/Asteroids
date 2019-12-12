e5ca07509098fe75fef5ac052f97785302fad1ac
ant478 <ant478@gmail.com>
2019-12-12 23:26:27 +0300

    Updates

---
+++ precache-manifest.2e1a28a6267813d0c9e07104d59fc421.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4661812f4bfeeff64f20e0f789f017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b33e60e635a73c847b12",
+    "url": "/static/css/main.4e7c3e7e.chunk.css"
+  },
+  {
+    "revision": "d681e8c1b00888b68a01",
+    "url": "/static/js/2.fac48b53.chunk.js"
+  },
+  {
+    "revision": "b33e60e635a73c847b12",
+    "url": "/static/js/main.dedc1430.chunk.js"
+  },
+  {
+    "revision": "495edc76f21232ce372e",
+    "url": "/static/js/runtime-main.716b101b.js"
+  },
+  {
+    "revision": "af88b17e39bb2b3dd43b356757f893f5",
+    "url": "/static/media/asteroid_face.af88b17e.png"
+  },
+  {
+    "revision": "5f84e9b843cfac9c05a314eb108973aa",
+    "url": "/static/media/bg-image.5f84e9b8.jpg"
+  },
+  {
+    "revision": "413c87332da8095d933cf51f02c83f67",
+    "url": "/static/media/logo-icon.413c8733.png"
+  },
+  {
+    "revision": "9b034cbc58c86b5dfa30edba35b81ea6",
+    "url": "/static/media/spinner.9b034cbc.png"
+  }
+]);
